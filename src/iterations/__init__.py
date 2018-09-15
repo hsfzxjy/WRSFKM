78a2540447078e2efefaf1bb1eb6df5f4b476b4e
huyirong <huyirong@mail.ustc.edu.cn>
2018-09-15 11:32:33 +0800

    afkm and sfkm

--- src/iterations/__init__.py
+++ src/iterations/__init__.py
@@ -3,28 +3,32 @@ from init import init_uv
 
 def run(X, labels, p, logger):
 
-    C = p.C
+        C = p.C
 
-    assert isinstance(p.iter_method, str)
+        assert isinstance(p.iter_method, str)
 
-    if p.init == 'preset':
-        U, V = p.initial
-    else:
-        U, V = init_uv(X, C, method=p.init)
+        if p.init == 'preset':
+                U, V = p.initial
+        else:
+                U, V = init_uv(X, C, method=p.init)
 
-    iter_method = p.iter_method.lower()
+        iter_method = p.iter_method.lower()
 
-    if iter_method in ['sv', 'mv']:
-        from iterations.normal import iteration
-    elif iter_method == 'aa':
-        from iterations.anderson import iteration
-    elif iter_method == 'orig':
-        from iterations.orig import iteration
-    elif iter_method == 'kmeans':
-        from iterations.kmeans import iteration
-    elif iter_method == 'fkm':
-        from iterations.fkm import iteration
-    else:
-        raise RuntimeError('Unknown method.')
+        if iter_method in ['sv', 'mv']:
+                from iterations.normal import iteration
+        elif iter_method == 'aa':
+                from iterations.anderson import iteration
+        elif iter_method == 'orig':
+                from iterations.orig import iteration
+        elif iter_method == 'kmeans':
+                from iterations.kmeans import iteration
+        elif iter_method == 'fkm':
+                from iterations.fkm import iteration
+        elif iter_method== 'afkm':
+                from iterations.afkm import iteration
+        elif iter_method=='sfkm':
+                from iterations.sfkm import iteration
+        else:
+                raise RuntimeError('Unknown method.')
 
-    return iteration(X, U, V, labels, p, logger=logger)
+        return iteration(X, U, V, labels, p, logger=logger)
